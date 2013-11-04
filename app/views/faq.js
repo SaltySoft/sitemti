7a54b989b98de318564cf3fa00c9c661a0d5d65b
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-11-04 18:25:08 +0100

    * redo faq responsive design with simple template call.

--- app/views/faq.js
+++ app/views/faq.js
@@ -3,12 +3,14 @@ define([
     'underscore',
     'backbone',
     'carousel',
-    'text!/templates/faq/faq.html',
-    'text!/templates/faq/faq_items.html',
     'text!/templates/faq/faqTemplate1.html',
     'text!/templates/faq/faqTemplate2.html',
-    'text!/templates/faq/faqTemplate3.html'
-], function ($, _, Backbone, Carousel, faqTemplate, faqItems, faqTemplate1, faqTemplate2, faqTemplate3) {
+    'text!/templates/faq/faqTemplate3.html',
+    'text!/templates/faq/faq-2-template1.html',
+    'text!/templates/faq/faq-2-template2.html',
+    'text!/templates/faq/faq-2-template3.html',
+    'text!/templates/faq/faq-2-template4.html'
+], function ($, _, Backbone, Carousel, faqTemplate1, faqTemplate2, faqTemplate3, faq2Template1, faq2Template2, faq2Template3, faq2Template4) {
     var View = Backbone.View.extend({
         tagName:"div",
         className:"slide",
@@ -39,40 +41,17 @@ define([
             base.$el.attr("data-url", "#" + base.page.urls[base.page.urls.length - 1]);
 
             var div = $(document.createElement("div"));
-            console.log("hauteur carousel faq1", base.$el.height());
             var templates_list = [];
-            console.log("1");
             if (base.$el.height() < 650) {
                 div.addClass("faq-container");
                 div.addClass("two-elements");
-                console.log("LESS THAN 650");
-                var elt = $('<div>' + faqItems + '</div>').filter(".faq-question-container");
-                var counter = 1;
-                elt.each(function () {
-                    var elt = $(this);
-                    if (counter == 1) {
-                        var faq_template = _.template(faqTemplate, {
-                        });
-                        var tpl = $(faq_template);
-                        tpl.find(".faq-column1").html(elt.html());
-                        templates_list.push(tpl);
-                        counter++;
-                    }
-                    if (counter == 2) {
-                        $(templates_list[templates_list.length - 1]).find(".faq-column2").html(elt.html());
-                        counter = 1;
-                    }
-                });
+                templates_list = [faq2Template1, faq2Template2, faq2Template3, faq2Template4];
             }
             else {
                 div.addClass("faq-container");
                 div.addClass("three-elements");
                 templates_list = [faqTemplate1, faqTemplate2, faqTemplate3];
             }
-
-            console.log("templates_list1", templates_list);
-            var templates_list2 = [faqTemplate1, faqTemplate2, faqTemplate3];
-            console.log("templates_list2", templates_list2);
 
             base.carousel = new Carousel();
             div.html(base.carousel.$el);
@@ -93,7 +72,6 @@ define([
 
             base.bind('resizeFaqEnd', function () {
                 //resizing stuff
-                console.log("hauteur carousel faq", base.$el.height());
                 if (base.$el.height() < 650) {
                     base.renderCarousel();
                 }
